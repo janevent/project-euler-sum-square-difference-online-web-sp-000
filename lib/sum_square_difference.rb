283b3eaae1a854805615b3bc4020401bc1037e4f
Jane Venter <janeanneventer@gmail.com>
2020-09-21 20:29:09 +0000

    Automatically backed up by Learn

--- lib/sum_square_difference.rb
+++ lib/sum_square_difference.rb
@@ -1,5 +1,6 @@
 # Implement your procedural solution here!
 def sum_square_difference(number)
+<<<<<<< HEAD
   
   sum_of_squares = (2..number).reduce(1) do |sum, n|
     sum + (n*n) 
@@ -11,4 +12,17 @@ def sum_square_difference(number)
   squared_sums *= squared_sums 
   puts squared_sums
   difference = squared_sums - sum_of_squares
+=======
+  squared_sums = 0
+  sum_of_squares = 0
+  difference = 0
+  sum_of_squares = (1..number).reduce(0) do |sum, n|
+    sum + (n*n) 
+  end
+  squared_sums = (1..number).reduce(0) do |sum, n| 
+    sum + n
+  end
+  squared_sums *= squared_sums 
+  difference = sum_of_squares - squared_sums
+>>>>>>> aca371af8a9634ff1b8031b0d0894793f90664df
 end
